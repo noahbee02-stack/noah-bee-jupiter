14472a6acb0e20cd0a2a2e706862198b45d1d33d
Noah Bouey <Noah.Bouey@ChildNet.us>
2026-08-23 16:05:43 -0400

    added message form functionality

--- js/index.js
+++ js/index.js
@@ -29,3 +29,41 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+const messageForm = document.querySelector('form[name="leave_message"]');
+
+messageForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const name = event.target.usersName.value;
+    const email = event.target.usersEmail.value;
+    const message = event.target.usersMessage.value;
+
+    console.log(name);
+    console.log(email);
+    console.log(message);
+
+    const messageSection = document.getElementById("messages");
+
+    const messageList = messageSection.querySelector("ul");
+
+    const newMessage = document.createElement("li");
+
+    newMessage.innerHTML = `<a href="mailto:${email}">${name}</a>: <span>${message}</span>`;
+
+    const removeButton = document.createElement("button");
+
+    removeButton.innerText = "remove";
+
+    removeButton.type = "button";
+
+    removeButton.addEventListener("click", function() {
+        const entry = removeButton.parentNode;
+        entry.remove();
+    });
+
+    newMessage.appendChild(removeButton);
+
+    messageList.appendChild(newMessage);
+
+    event.target.reset();
+});
